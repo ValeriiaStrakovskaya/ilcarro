0ca001682892a660336942c4e0e757cfa2f9857c
ValeriiaStrakovsky <37975295+ValeriiaStrakovskaya@users.noreply.github.com>
2022-07-10 10:46:20 +0300

    New Comm

---
+++ src/test/java/Tests.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Tests {
+    WebDriver wd;
+
+
+
+    @BeforeMethod
+    public void preCondition(){
+        WebDriverManager.chromedriver().setup();
+        wd = new ChromeDriver();
+        wd.navigate().to("https://ilcarro-1578153671498.web.app/search");
+       // wd.get("https://ilcarro-1578153671498.web.app/search");
+    }
+
+    @Test
+    public void testURL(){}
+
+
+}

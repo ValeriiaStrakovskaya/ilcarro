85c81c4ec380511ad95780f5b99e1b4acfa8cdf5
ValeriiaStrakovsky <37975295+ValeriiaStrakovskaya@users.noreply.github.com>
2022-08-02 16:13:13 +0300

    HW_10

--- src/test/java/test/AddNewCarTests.java
+++ src/test/java/test/AddNewCarTests.java
@@ -53,7 +53,69 @@ public class AddNewCarTests extends TestBase{
 //        app.getCar().attachPhoto();
 //     app.getUser().submitForm();
     }
+    @Test
+    public void addNewCarTestNegative(){
+        //validation test
+        app.getCar().openCarForm();
+        app.getCar().click(By.xpath("//input[@id='make']"));
+        app.getCar().click(By.xpath("//input[@id='model']"));
+        Assert.assertTrue(app.getCar().isElementPresent(By.xpath("//div[contains(text(),'Make is required')]")));
+    }
 
-   
+    @Test
+    public void addNewCarTestNegative2(){
+
+        //ALL required fields should be filled.Photo isn't attached
+        int i = (int)((System.currentTimeMillis()/1000)%3600);
+
+        Car car2 = Car.builder()
+                .address("Haifa")
+                .make("KIA")
+                .model("Sportage")
+                .year("2020")
+                .engine("2.3")
+                .fuel("Petrol")
+                .gear("AT")
+                .wD("AWD")
+                .doors("5")
+                .seats("5")
+                .clasS("C")
+                .fuelConsumption("8.9")
+                .carRegNumber("100-200-" + i)
+                .price("150")
+                .distanceInclude("500")
+                .typeFeature("Climate control")
+                .about("Like new")
+                .build();
+
+        app.getCar().openCarForm();
+        app.getCar().fillCarForm(car2);
+        Assert.assertFalse(app.getUser().isButtonClickable(By.xpath("//button[@type='submit']")));
+
+    }
+
+    @Test
+    public void addNewCarTestPositiveForFeatures(){
+
+        app.getCar().openCarForm();
+        app.getCar().click(By.xpath("//input[@placeholder='Type feature']"));
+        app.getCar().type(By.xpath("//input[@placeholder='Type feature']"),"Something");
+        app.getCar().click(By.cssSelector("#about"));
+        app.getCar().click(By.cssSelector("button[type$='button']"));
+        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//input[position()=2]")));
+
+    }
+    @Test
+    public void addNewCarTestNegativeForFeatures(){
+
+        app.getCar().openCarForm();
+        app.getCar().click(By.xpath("//input[@placeholder='Type feature']"));
+        app.getCar().type(By.xpath("//input[@placeholder='Type feature']"),"");
+        app.getCar().click(By.cssSelector("#about"));
+        Assert.assertFalse(app.getUser().isButtonClickable(By.cssSelector("button[type$='button']")));
+        Assert.assertFalse(app.getUser().isElementPresent(By.xpath("//input[position()=2]")));
+
+    }
+
 }
 
